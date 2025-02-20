25ac0fc0f38cff33e5cec973d8368792adc9af42
Abin Thomas <followgamer102@gmail.com>
2025-02-20 22:10:44 +0530

    d1 -> neonDB

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,27 @@
-import type { D1Database } from "@cloudflare/workers-types";
-export interface Env {
-    DB: D1Database;
+import { neon } from "@neondatabase/serverless";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("Missing DATABASE_URL in environment variables.");
 }
+
+export const db = neon(process.env.DATABASE_URL);
+
+const schema = async () => {
+  try {
+    await db`
+      CREATE TABLE IF NOT EXISTS DONORS (
+        id SERIAL PRIMARY KEY,
+        name TEXT NOT NULL,
+        phoneNumber TEXT NOT NULL,
+        address TEXT NOT NULL,
+        email TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL
+      )
+    `;
+    console.log("Table created successfully.");
+  } catch (error) {
+    console.error("Error creating table:", error);
+  }
+};
+
+schema();
